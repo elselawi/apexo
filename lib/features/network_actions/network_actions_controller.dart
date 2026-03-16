35d39da894148e615ddabd5499b3c706dc33e468
elselawi <alielselawi@gmail.com>
2026-03-16 21:14:55 +0300

    manually request notifications permission for the web

--- lib/features/network_actions/network_actions_controller.dart
+++ lib/features/network_actions/network_actions_controller.dart
@@ -1,13 +1,17 @@
 import 'package:apexo/app/routes.dart';
+import 'package:apexo/common_widgets/button_styles.dart';
 import 'package:apexo/common_widgets/no_items_found.dart';
 import 'package:apexo/features/login/login_controller.dart';
 import 'package:apexo/features/settings/settings_stores.dart';
 import 'package:apexo/services/launch.dart';
+import 'package:apexo/services/localization/locale.dart';
 import 'package:apexo/services/network.dart';
-import 'package:apexo/services/notifications.dart';
+import 'package:apexo/services/notifications/static_notifications.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/foundation.dart';
 import '../../services/login.dart';
 import '../../core/observable.dart';
+import 'package:apexo/utils/js/js_bridge.dart';
 
 class NetworkAction {
   String tooltip;
@@ -64,8 +68,12 @@ class _NetworkActions {
       NetworkAction(
         tooltip: "Notifications",
         activeColor: Colors.blue,
-        badge: notifications.notifications.isNotEmpty ? notifications.notifications.length.toString() : null,
+        badge: notifications.notifications.isNotEmpty
+            ? notifications.notifications.length.toString()
+            : null,
         processing: notifications.notifications.isNotEmpty,
+        // TODO: find more logical place to place this widgets
+        // shouldn't be inside a controller!
         icon: FlyoutTarget(
           controller: _notificationsFlyoutController,
           child: const Icon(FluentIcons.ringer),
@@ -78,89 +86,117 @@ class _NetworkActions {
             builder: (ctx) {
               return FlyoutContent(
                 useAcrylic: false,
-                constraints: const BoxConstraints(maxWidth: 300, maxHeight: 360),
-                child: notifications.notifications.isEmpty ? const NoItemsFound() : ListView(
-                  children: notifications.notifications.map((notification) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: notifications.notifications.last.title ==
-                                    notification.title
-                                ? Colors.transparent
-                                : FluentTheme.of(ctx)
-                                    .inactiveColor
-                                    .withValues(alpha: 0.2),
-                          ),
-                        ),
-                      ),
-                      width: 280,
-                      padding: const EdgeInsets.all(8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        mainAxisSize: MainAxisSize.min,
-                        spacing: 5,
-                        children: [
-                          Row(
-                            spacing: 5,
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(8),
+                constraints:
+                    const BoxConstraints(maxWidth: 300, maxHeight: 360),
+                child: notifications.notifications.isEmpty
+                    ? const NoItemsFound()
+                    : ListView(
+                        children: notifications.notifications
+                            .map((notification) {
+                              return Container(
                                 decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.circular(40),
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: notifications
+                                                  .notifications.last.title ==
+                                              notification.title
+                                          ? Colors.transparent
+                                          : FluentTheme.of(ctx)
+                                              .inactiveColor
+                                              .withValues(alpha: 0.2),
+                                    ),
+                                  ),
                                 ),
-                                child: Icon(notification.icon,
-                                    color: Colors.white),
-                              ),
-                              SizedBox(
-                                width: 165,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                width: 280,
+                                padding: const EdgeInsets.all(8),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.min,
+                                  spacing: 5,
                                   children: [
-                                    Text(
-                                      notification.title,
-                                      style: FluentTheme.of(ctx)
-                                          .typography
-                                          .bodyStrong,
-                                      overflow: TextOverflow.ellipsis,
+                                    Row(
+                                      spacing: 5,
+                                      children: [
+                                        Container(
+                                          padding: const EdgeInsets.all(8),
+                                          decoration: BoxDecoration(
+                                            color: Colors.grey,
+                                            borderRadius:
+                                                BorderRadius.circular(40),
+                                          ),
+                                          child: Icon(notification.icon,
+                                              color: Colors.white),
+                                        ),
+                                        SizedBox(
+                                          width: 165,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                notification.title,
+                                                style: FluentTheme.of(ctx)
+                                                    .typography
+                                                    .bodyStrong,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                              Text(
+                                                notification.body,
+                                                style: FluentTheme.of(ctx)
+                                                    .typography
+                                                    .caption,
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                    Text(
-                                      notification.body,
-                                      style: FluentTheme.of(ctx)
-                                          .typography
-                                          .caption,
-                                      maxLines: 2,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
+                                    ...notification.actions
+                                        .where((a) =>
+                                            a.hideOnRoute !=
+                                            routes.currentRoute.identifier)
+                                        .map((action) {
+                                      return HyperlinkButton(
+                                        onPressed: () {
+                                          action.action();
+                                          _notificationsFlyoutController
+                                              .close();
+                                        },
+                                        child: Text(action.label),
+                                      );
+                                    }),
                                   ],
                                 ),
-                              ),
+                              );
+                            })
+                            .cast<Widget>()
+                            .toList()
+                          ..addAll([
+                            if (kIsWeb) ...[
+                              const Divider(),
+                              HyperlinkButton(
+                                onPressed: () {
+                                  JSBridge.setGlobalVariable(
+                                    "shouldShowPrompt",
+                                    "yes",
+                                  );
+                                  _notificationsFlyoutController.close();
+                                },
+                                child: ButtonContent(
+                                  FluentIcons.ringer_active,
+                                  txt("enableNotifications"),
+                                ),
+                              )
                             ],
-                          ),
-                          ...notification.actions
-                              .where((a) =>
-                                  a.hideOnRoute !=
-                                  routes.currentRoute.identifier)
-                              .map((action) {
-                            return HyperlinkButton(
-                              onPressed: () {
-                                action.action();
-                                _notificationsFlyoutController.close();
-                              },
-                              child: Text(action.label),
-                            );
-                          }),
-                        ],
+                          ]),
                       ),
-                    );
-                  }).toList(),
-                ),
               );
             },
           );
         },
-
       ),
       NetworkAction(
         tooltip: "Theme",

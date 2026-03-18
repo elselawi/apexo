ac976728a13640d904159093598ef74070eb5896
elselawi <alielselawi@gmail.com>
2026-03-18 21:45:27 +0300

    hide notifications before login

--- lib/features/network_actions/network_actions_widget.dart
+++ lib/features/network_actions/network_actions_widget.dart
@@ -3,6 +3,7 @@ import 'package:apexo/features/login/login_controller.dart';
 import 'package:apexo/features/network_actions/network_actions_controller.dart';
 import 'package:apexo/common_widgets/transitions/rotate.dart';
 import 'package:apexo/features/settings/settings_stores.dart';
+import 'package:apexo/services/launch.dart';
 import 'package:apexo/services/network.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 
@@ -18,11 +19,14 @@ class NetworkActions extends StatelessWidget {
             loginCtrl.proceededOffline.stream,
             network.isOnline.stream,
             localSettings.stream,
+            launch.open.stream
           ],
           builder: (context, _) {
             return Wrap(
               children: [
-                ...networkActions.actions.where((action) => action.hidden != true).map(
+                ...networkActions.actions
+                    .where((action) => action.hidden != true)
+                    .map(
                       (action) => Container(
                         margin: const EdgeInsets.only(left: 3, top: 7),
                         child: Stack(
@@ -53,11 +57,15 @@ class NetworkActions extends StatelessWidget {
             onPressed: action.onPressed,
             iconButtonMode: IconButtonMode.large,
             style: ButtonStyle(
-                shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(50))),
+                shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50))),
                 iconSize: WidgetStateProperty.all(18),
-                foregroundColor: WidgetStatePropertyAll(action.processing ?? false ? Colors.white : null),
-                backgroundColor:
-                    WidgetStatePropertyAll(action.processing ?? false ? action.activeColor : Colors.transparent)),
+                foregroundColor: WidgetStatePropertyAll(
+                    action.processing ?? false ? Colors.white : null),
+                backgroundColor: WidgetStatePropertyAll(
+                    action.processing ?? false
+                        ? action.activeColor
+                        : Colors.transparent)),
           ),
         ),
       ),
@@ -76,7 +84,9 @@ class NetworkActions extends StatelessWidget {
           borderRadius: BorderRadius.circular(50),
           boxShadow: kElevationToShadow[2],
         ),
-        child: Center(child: Text(action.badge ?? "", style: const TextStyle(fontSize: 10, color: Colors.grey))),
+        child: Center(
+            child: Text(action.badge ?? "",
+                style: const TextStyle(fontSize: 10, color: Colors.grey))),
       ),
     );
   }

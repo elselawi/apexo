b51147af58b438114afc5cc6f7af15769cdfe386
Ali A. Saleem <alielselawi@gmail.com>
2026-05-15 10:33:57 +0300

    prevent multiple instances from running

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -3,6 +3,21 @@ import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
+    let runningApps = NSRunningApplication.runningApplications(withBundleIdentifier: Bundle.main.bundleIdentifier!)
+    if runningApps.count > 1 {
+      self.orderOut(nil) // Hide the window while the alert is active
+      
+      let alert = NSAlert()
+      alert.messageText = "Error"
+      alert.informativeText = "The application is already running, Please quit."
+      alert.alertStyle = .critical
+      alert.addButton(withTitle: "OK")
+      alert.runModal()
+      
+      NSApp.terminate(nil)
+      return
+    }
+
     let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
